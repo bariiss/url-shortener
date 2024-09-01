7f33100001852f0b6e01dfa57910ff279e0e3047
bariiss <barisdogu@outlook.de>
2024-09-01 13:24:07 +0300

    ... #build

---
+++ internal/mem.go
@@ -0,0 +1,52 @@
+package internal
+
+import (
+	"fmt"
+    "log"
+
+    "github.com/redis/go-redis/v9"
+)
+
+var (
+    rdb           *redis.Client
+)
+
+func InitMem() {
+	if redisActive {
+		rdb = redis.NewClient(&redis.Options{
+			Addr: redisAddr,
+		})
+
+		if _, err := rdb.Ping(ctx).Result(); err != nil {
+			log.Fatalf("Error connecting to Redis: %v", err)
+		}
+		log.Println("Connected to Redis")
+		return
+	}
+
+	memoryStorage = make(map[string]string)
+	log.Println("Redis is not active. Using in-memory storage.")
+}
+
+func setURL(shortURL, originalURL string) error {
+	if redisActive {
+		return rdb.Set(ctx, shortURL, originalURL, 0).Err()
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	memoryStorage[shortURL] = originalURL
+	return nil
+}
+
+func getURL(shortURL string) (string, error) {
+	if redisActive {
+		return rdb.Get(ctx, shortURL).Result()
+	}
+	mu.RLock()
+	defer mu.RUnlock()
+	originalURL, exists := memoryStorage[shortURL]
+	if !exists {
+		return "", fmt.Errorf("URL not found")
+	}
+	return originalURL, nil
+}
